24baceab4aaee4b7bd945d99e901491ee43130f6
Miles Watson <contact@mileswatson.net>
2022-01-01 17:36:31 +0000

    Passed game 3.

--- test/game3Test.js
+++ test/game3Test.js
@@ -1,7 +1,7 @@
 const { assert } = require("chai");
 
-describe("Game3", function() {
-  it("should be a winner", async function() {
+describe("Game3", function () {
+  it("should be a winner", async function () {
     const Game = await ethers.getContractFactory("Game3");
     const game = await Game.deploy();
     await game.deployed();
@@ -12,14 +12,20 @@ describe("Game3", function() {
     // hardhat will create 10 accounts for you by default
     // you can get one of this accounts with ethers.provider.getSigner
     // and passing in the zero-based indexed of the signer you want:
-    const signer = ethers.provider.getSigner(0);
-    const address = await signer.getAddress();
+
+    const signers = [...Array(3).keys()].map(x => ethers.provider.getSigner(x));
+
+    const addresses = await Promise.all(signers.map(x => x.getAddress()));
+    console.log(addresses);
+
 
     // to call a contract as a signer you can use contract.connect
-    await game.connect(signer).buy({ value: "1" });
+    await game.connect(signers[0]).buy({ value: 2 });
+    await game.connect(signers[1]).buy({ value: 3 });
+    await game.connect(signers[2]).buy({ value: 1 });
 
     // TODO: win expects three arguments
-    await game.win();
+    await game.win(addresses[0], addresses[1], addresses[2]);
 
     // leave this assertion as-is
     assert(await game.isWon(), "You did not win the game");
