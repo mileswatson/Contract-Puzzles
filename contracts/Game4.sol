9cc25f61b100e631243f613228f0aa27a25e62fe
Miles Watson <contact@mileswatson.net>
2022-01-01 17:09:10 +0000

    Formatting.

--- contracts/Game4.sol
+++ contracts/Game4.sol
@@ -1,18 +1,18 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.0;
 
 contract Game4 {
-  bool public isWon;
+    bool public isWon;
 
-  mapping(address => mapping(address => bool)) nested;
+    mapping(address => mapping(address => bool)) nested;
 
-  function write(address x) external {
-    nested[x][msg.sender] = true;
-  }
+    function write(address x) external {
+        nested[x][msg.sender] = true;
+    }
 
-  function win(address y) external {
-    require(nested[msg.sender][y], "Nope. Try again!");
+    function win(address y) external {
+        require(nested[msg.sender][y], "Nope. Try again!");
 
-    isWon = true;
-  }
+        isWon = true;
+    }
 }
