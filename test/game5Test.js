1b4701a83482954dd1ae28c6503c0dc5b03ef7bf
Miles Watson <contact@mileswatson.net>
2022-01-02 16:19:50 +0000

    Passed game 5.

--- test/game5Test.js
+++ test/game5Test.js
@@ -1,14 +1,22 @@
 const { assert } = require("chai");
 
-describe("Game5", function() {
-  it("should be a winner", async function() {
+describe("Game5", function () {
+  it("should be a winner", async function () {
     const Game = await ethers.getContractFactory("Game5");
     const game = await Game.deploy();
     await game.deployed();
 
-    // good luck
+    let signer;
+    do {
+      signer = ethers.Wallet.createRandom();
+    } while (BigInt(await signer.getAddress()) >= BigInt("0x00fFfffFFffFFFfFFfFFffffFffffffffFFfffFf"));
 
-    await game.win();
+    const address = await signer.getAddress();
+    console.log(address);
+    ethers.provider.getSigner(0).sendTransaction({ to: address, value: ethers.utils.parseEther("1") });
+
+    // good luck
+    await game.connect(signer.connect(ethers.provider)).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), "You did not win the game");
